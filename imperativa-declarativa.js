d01a07d15cc23323488d2fcd943029f12f19f68a
Leandro Araújo <leandro.arauj047.edu@gmail.com>
2022-04-18 12:40:13 -0300

    Adicionando

---
+++ imperativa-declarativa.js
@@ -0,0 +1,17 @@
+// Função que eleva o número ao quadrado
+
+// Imperativa:
+let number = 2
+
+function square() {
+  return number ** 2
+}
+
+let numberQ = square()
+
+console.log(number)
+console.log(numberQ)
+
+// Declarativa: o que fazer e não como fazer.
+const square = n => n*n
+
